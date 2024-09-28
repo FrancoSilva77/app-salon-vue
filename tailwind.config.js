f62d98b244766a29bc70c05400a39974f4e2cb90
FrancoSilva <franco.s.satoritech@gmail.com>
2024-09-27 20:12:59 -0600

    style(App):bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        app: "url('/img/1.jpg')"
+      }
+    }
   },
   plugins: []
 }
